489046966bbe3207ec8ad4659779a01d10887bb4
Andreas <Fyziikme@gmail.com>
2020-09-10 09:06:01 +0200

    First commit

---
+++ src/main/java/kea/andr561d/Andr561dApplication.java
@@ -0,0 +1,13 @@
+package kea.andr561d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Andr561dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Andr561dApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/andr561d/Andr561dApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.andr561d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Andr561dApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
